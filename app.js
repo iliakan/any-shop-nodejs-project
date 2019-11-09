5578c31551e7cf9da1d4229b88d503c17b07c6bd
Ilya Kantor <iliakan@gmail.com>
2019-11-09 22:30:28 +0300

    WIP

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const config = require('./config');
-
+const path = require('path');
+const send = require('koa-send');
 const Router = require('@koa/router');
 
 /*
@@ -29,43 +30,16 @@ require('./handlers/error')(app);
 // uncomment to enable CORS from anywhere
 // app.use(require('@koa/cors')({maxAge: 86400}));
 
-const router = new Router({prefix: '/api'});
-router.get('/stats/:field(orders|sales|customers)', async (ctx) => {
-  let orders = app.db.get('orders');
-  if (ctx.query.gte) {
-    orders = orders.filter(order => order.createdAt >= new Date(ctx.query.gte));
+app.use(async (ctx, next) => {
+  if (!ctx.url.startsWith('/api') && !ctx.url.includes('.')) {
+    await send(ctx, 'index.html', {root: config.publicRoot});
+  } else {
+    await next();
   }
-  if (ctx.query.lte) {
-    orders = orders.filter(order => order.createdAt <= new Date(ctx.query.lte));
-  }
-  let ordersCountByDate = Object.create(null);
-
-  let customersSet = new Set();
-  for(let order of orders) {
-    // console.log(order);
-    let dateStr = order.createdAt.toISOString().replace(/T.*/, '');
-    if (!ordersCountByDate[dateStr]) ordersCountByDate[dateStr] = 0;
-    switch(ctx.params.field) {
-      case 'orders':
-        ordersCountByDate[dateStr]++;
-        break;
-      case 'sales':
-        ordersCountByDate[dateStr] += order.amount;
-        break;
-      case 'customers':
-        if (!customersSet.has(order.phone)) {
-          customersSet.add(order.phone);
-          ordersCountByDate[dateStr]++;
-        }
-        break;
-      default:
-        throw new Error("Must never happen");
-    }
-  }
-
-  console.log(ordersCountByDate);
-  ctx.body = ordersCountByDate;
 });
+
+const router = new Router();
+router.get('/api/stats/:field(orders|sales|customers)', require('./controllers/api/stats'));
 
 app.use(router.routes());
 

b70db0a46a17f817033591635a391db9708c662f
Ilya Kantor <iliakan@gmail.com>
2019-11-25 10:21:09 +0300

    WIP

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,7 @@
 const app = require('../app');
 const config = require('../config');
 
-let server = app.listen(config.server.port, config.server.host, () => {
+app.listen(config.server.port, config.server.host, () => {
   console.log(`App is running on http://${config.server.host}:${config.server.port}`);
 });
 

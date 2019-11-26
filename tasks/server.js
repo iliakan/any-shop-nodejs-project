7bdf626d37eccff5f399e3e8005b4e1866941529
Ilya Kantor <iliakan@gmail.com>
2019-11-26 09:45:51 +0300

    WIP

--- tasks/server.js
+++ tasks/server.js
@@ -3,7 +3,7 @@ const config = require('../config');
 
 module.exports = async function() {
   let server = app.listen(config.server.port, () => {
-    console.log(`App is running on http://${config.server.host}:${config.server.port}`);
+    console.log(`App is running on port ${config.server.port}`);
   });
 
   await new Promise(res => server.on('close', res));
